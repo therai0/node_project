f0b0e9571fd476f362b3702cc140bf9da24508c4
bhaskar <raibhaskar003@gmail.com>
2024-11-23 20:10:40 +0545

    Change is port

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import experss from "express";
 
 const app = experss();
 
-const port = 8000;
+
 
 app.get("/",(req,res)=> {
     return res.status(200).json({message:"hello world"});
 });
 
 
-app.listen(port,()=> {
+app.listen(8000,()=> {
     console.log("App is running at", port);
 })
