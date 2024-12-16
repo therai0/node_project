e6f4a5c18c8094a5b7148d760522203e2adbb8b4
bhaskar <raibhaskar003@gmail.com>
2024-12-16 12:49:41 +0545

    Adding key in api

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res)=> {
- return res.status(200).json({
-
-    message:"Hello world",
-    username:"DBsir",
-    firstname:"Bhaskar",
-    lastname:"Rai",
-    education:"Pursuing bachelor of Information Technology at Balmiki lincoln college",
-    skills:"HTML,CSS,JS,React js and node js",
-    email:"imraibhaskar@gmail.com",
-    contact:"+977 9815048958"
- });
+    const key = req.params.key;
+    if(key == 123) 
+    {
+        return res.status(200).json({
+       
+           message:"Hello world",
+           username:"DBsir",
+           firstname:"Bhaskar",
+           lastname:"Rai",
+           education:"Pursuing bachelor of Information Technology at Balmiki lincoln college",
+           skills:"HTML,CSS,JS,React js and node js",
+           email:"imraibhaskar@gmail.com",
+           contact:"+977 9815048958"
+        });
+    }
+    else {
+        res.status(401).json({message:"unauthorize access"});
+    }
 })
 
 app.listen(8080,()=> {
