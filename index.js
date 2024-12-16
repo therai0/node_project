8dea2dc9decaf1ef94307d93b1587687306415c7
bhaskar <raibhaskar003@gmail.com>
2024-12-16 12:34:38 +0545

    Changes in index.js file

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res)=> {
- return res.status(200).json({"message":"Hello world"});
+ return res.status(200).json({
+
+    message:"Hello world",
+    username:"DBsir",
+    firstname:"Bhaskar",
+    lastname:"Rai",
+    education:"Pursuing bachelor of Information Technology at Balmiki lincoln college",
+    skills:"HTML,CSS,JS,React js and node js",
+    email:"imraibhaskar@gmail.com",
+    contact:"+977 9815048958"
+ });
 })
 
 app.listen(8080,()=> {
